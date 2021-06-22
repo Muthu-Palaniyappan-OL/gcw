05dfb4c807dbea4f639f5e0dda273d612041dc3e
Muthu Palaniyappan OL <muthu892542@gmail.com>
2021-06-22 08:48:43 +0530

    Footer Non Styled

---
+++ common/footer.php
@@ -0,0 +1,55 @@
+<footer>
+
+    <div class="navigation_footer_grid">
+        <ul>
+            <a href=""><li>About Us</li></a>
+            <a href=""><li>Products & Solutions</li></a>
+            <a href=""><li>Investor</li></a>
+            <a href=""><li>Media</li></a>
+            <a href=""><li>Careers</li></a>
+            <a href=""><li>Contact Us</li></a>
+        </ul>
+    </div>
+
+
+    <div class="newsletter_footer_grid">
+        <form action="index.php" method="post">
+            <input type="text" name="email_newsletter"  id="email_newsletter">
+            <input type="submit" value="Submit" name="newsletter_submit">
+        </form>
+    </div>
+
+
+    <div class="social_media_footer_grid">
+        <a href="#">
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+        <a href="#">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+        <a href="#">
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a href="#">
+            <i class="fa fa-linkedin" aria-hidden="true"></i>
+        </a>
+    </div>
+
+
+    <div class="contact_us_footer_grid">
+        <address>
+            19th Floor, HBC Towers, Mount Road, Chennai 560001, India
+        </address>
+        <p>Tel: +91 8976236789</p>
+    </div>
+
+
+    <div class="copy_right_footer_grid">&copy; GCW 2021</div>
+
+
+    <div class="policy_footer_grid">
+        <a href="#">Privacy Policy</a><a href="#">Terms of Use</a><a href="#">Legal</a>
+    </div>
+
+
+</footer>
