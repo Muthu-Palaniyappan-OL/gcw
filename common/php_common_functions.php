ece0c87d686ee692b3bde35e7c306accc3384b84
Muthu Palaniyappan OL <muthu892542@gmail.com>
2021-07-02 16:57:27 +0530

    Fixed Active Users Bug

--- common/php_common_functions.php
+++ common/php_common_functions.php
@@ -22,10 +22,11 @@ function recordUser($pagename){
         $sql ="UPDATE `visitor_count` SET visit_count=visit_count+1 WHERE `date`='".date("Y-m-d")."' AND `page`='".$pagename."';";
         mysqli_query($conn, $sql);
         // User $_SERVER['REMOTE_ADDR'] When deploying
+        echo $_SERVER["REMOTE_ADDR"];
         $_SESSION["IP"] = trim(shell_exec("dig +short myip.opendns.com @resolver1.opendns.com")); //reading public IP address
         $_SESSION["country"] = json_decode(file_get_contents("https://api.country.is/".$_SESSION["IP"]))->country;
         $_SESSION["userType"] = "normalUser"; //Declaring the user type whether admin or non admin
-        $_SESSION["Device"] = getallheaders()["User-Agent"]; //Gets browser and device details
+        $_SESSION["Device"] = $_SERVER["HTTP_USER_AGENT"]; //Gets browser and device details
         include "closeDbConn.php";
     }
     else{
